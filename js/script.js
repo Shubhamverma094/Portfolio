675cc7d7ed2439c8d444a1700a333d113ddfa069
Shubhamverma <shubhamverma08726@gmail.com>
2024-06-01 23:38:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let icon=document.querySelector(".icon");
+let ul= document.querySelector("ul");
+
+icon.addEventListener("click",()=>{
+    ul.classList.toggle("show");
+    console.log(ul);
+    
+})
